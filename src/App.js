a6cfa8817ff232405860b5dfd7b4acb7c2880bea
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2020-11-30 13:43:40 -0700

    Did App,Form and List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
 import React from 'react';
+import ReactDom from 'react-dom'
+
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+
+
+
+const toDo = [
+  
+    {
+      task: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    },
+    {
+      task: 'Set up Esxi',
+      id: 1528817084399,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084420,
+      completed: false
+    }
+  
+];
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {toDo}
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  handleToggle = (itemID)=> {
+    this.setState({
+      toDo: this.state.toDo.map(item=>{
+        if(itemID === item.id) {
+          return({
+            ...item,
+            completed: !item.completed
+          });
+        }else {
+          return(item);
+        }
+      })
+    });
+  }
+
+  handleAdd = task => {
+    this.setState({
+      toDo: [...this.state.toDo, { task: task, id: Date.now(), completed:false}]
+    })
+  }
+  
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>This is the Shit you NEED to get done.</h1>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,28 @@
+import React from "react";
+import Item from "./Todo";
+
+const TodoList = props => {
+    return (
+        <div className="todo-list">
+            {props.toDo.map(item => (
+                <Item key={item.id} item={item} handleToggle={props.handleToggle}/>
+            ))}
+            <button onClick={props.handleToggle}
+            className="clear-btn">
+                Clear Completed Items
+            </button>
+        </div>
+    );
+};
+
+
+export default TodoList;
+
+
+
+
+
+
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
